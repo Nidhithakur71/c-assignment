fda4154888a4995efe189e71ce2d828b815cbb6e
Nidhithakur71 <126388476+Nidhithakur71@users.noreply.github.com>
2023-03-25 00:23:06 +0530

    Add files via upload

---
+++ assign2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int x,y;
+	printf("enter the value of x\n");
+	scanf("%d",&x);
+	printf("enter the value of y\n");
+	scanf("%d",&y);
+	printf("sum is %d\n",x+y);
+	return 0;
+}
